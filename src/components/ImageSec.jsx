16344e3d645739225d169e678a47de8a3d78eaf5
unknown <iliyaaszahoor@gmail.com>
2025-06-08 21:57:25 +0530

    fixed sec2 (scrollingimgsection) now better scrolling\

--- src/components/ImageSec.jsx
+++ src/components/ImageSec.jsx
@@ -10,7 +10,7 @@ export default function ImageSec({ images, sectionClass, id }) {
 
   useGSAP(() => {
     gsap.to(imgRef.current, {
-      y: "-15%",
+      yPercent: -10,
       ease: "none",
       scrollTrigger: {
         trigger: `.${sectionClass}`,
@@ -26,12 +26,15 @@ export default function ImageSec({ images, sectionClass, id }) {
       className={`parallax-section ${sectionClass} w-full h-screen overflow-hidden relative`}
       style={{ clipPath: "polygon(0 0, 100% 0, 100% 100%, 0 91%)" }}
     >
-      <img
-        ref={imgRef}
-        src={images}
-        alt=""
-        className="absolute top-0 left-0 w-full h-[120%] object-cover"
-      />
+      <div className="flex justify-center items-center h-full">
+
+        <img
+          ref={imgRef}
+          src={images}
+          alt=""
+          className="absolute top-0 w-full h-[120%] object-cover"
+        />
+      </div>
     </div>
   );
 }
--- src/components/ScrollImageSection.jsx
+++ src/components/ScrollImageSection.jsx
@@ -11,7 +11,7 @@ export default function ScrollImageSection() {
       scrollTrigger: {
         trigger: '.sec2',
         start: 'top top',
-        end: "+=1200",
+        end: "+=3000",
         pin: true,
         scrub: true,
       }
@@ -21,28 +21,36 @@ export default function ScrollImageSection() {
       opacity: 0,
       scale: 1.1,
       ease: "none"
-    }, 0);
+    }, 0.25);
 
+    
     tlIII.to('.scrollimg1', {
       opacity: 1,
       scale: 1.1,
       ease: "none"
-    }, 0);
+    }, 0.25);
+
+    tlIII.to('.scrollimg1', {
+      opacity: 0,
+      ease: "none"
+    }, 0.95);
+
     
     tlIII.to('.scrollimg2', {
       opacity: 1,
       scale: 1.1,
       ease: 'none'
     }, 1);
+
   }, []);
 
   return (
-    <div className="sec2 h-[120vh] relative overflow-hidden bg-[#14131C]">
-      <div className="imgcont absolute inset-0" style={{ clipPath: "polygon(0 5%, 100% 0, 100% 100%, 0 96%)" }}
->
-        <img className="scrollimg absolute inset-0 w-full h-full object-cover opacity-100" src="./Boobie_Ike_02.jpg" />
-        <img className="scrollimg1 absolute inset-0 w-full h-full object-cover opacity-0" src="./Raul_Bautista_landscape.jpg" />
-        <img className="scrollimg2 absolute inset-0 w-full h-full object-cover opacity-0" src="./DreQuan_Priest_03.jpg" />
+    <div className="sec2 h-screen relative overflow-hidden bg-[#14131C] flex justify-center items-center">
+      <div className="imgcont absolute inset-0 h-[120vh] flex justify-center items-center" style={{ clipPath: "polygon(0 5%, 100% 0, 100% 100%, 0 96%)" }}
+      >
+        <img className="scrollimg absolute inset-0 w-full h-full object-cover object-center opacity-100" src="./Boobie_Ike_02.jpg" />
+        <img className="scrollimg1 absolute inset-0 w-full h-full object-cover object-center opacity-0" src="./Raul_Bautista_landscape.jpg" />
+        <img className="scrollimg2 absolute inset-0 w-full h-full object-cover object-center opacity-0" src="./DreQuan_Priest_03.jpg" />
       </div>
       <div className='w-full py-[20rem] px-10 bg-gradient-to-t from-[#14131C] to-transparent absolute bottom-0'></div>
     </div>
